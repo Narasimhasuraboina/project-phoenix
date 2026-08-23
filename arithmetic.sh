9900e9db7116001dd9a3888ef8c17f3e88f95537
Narasimhasuraboina <suraboinanarasimha@gmail.com>
2026-08-23 12:37:18 +0000

    23-08-26 today's all about basic arithmetic operations

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
 
-X=10
-Y=5
+cost_pineapple=50
+cost_banana=4
+cost_watermelon=23
+cost_basket=1
 
-# Addition
-SUM=$((X + Y))
-echo "Sum of $X and $Y is: $SUM"
-
-# Subtraction
-DIFF=$((X - Y))
-echo "Difference between $X and $Y is: $DIFF"
-
-# Multiplication
-PRODUCT=$((X * Y))
-echo "Product of $X and $Y is: $PRODUCT"
-
-# Division
-QUOTIENT=$((X / Y))
-echo "Quotient of $X divided by $Y is: $QUOTIENT"
-
-# Modulus (remainder)
-REMAINDER=$((X % Y))
-echo "Remainder of $X divided by $Y is: $REMAINDER"
-
-# Increment
-X=$((X + 1))
-echo "After incrementing, X is now: $X"
-
-# Decrement
-Y=$((Y - 1))
-echo "After decrementing, Y is now: $Y"
+total=$((cost_pineapple+(cost_banana*2)+(cost_watermelon*3)+cost_basket))
+echo "total : $total"
