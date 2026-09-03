450c91cf043bd64a6db9ad0ce6d1d2bea7c554c6
Narasimhasuraboina <suraboinanarasimha@gmail.com>
2026-09-03 18:10:40 +0000

    new changes inn commands.txt

---
+++ first.c
@@ -0,0 +1,16 @@
+#include<fcnti.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<string.h>
+int main(int argc,char, *args[]){
+    int fd=open("/tmp/file",O_WRONLY | O_CREAT | O_TRUNC,S_IWUSR | S_IWUSR);
+        assert(fd>=0);
+        char buffer[20];
+        sprintf(buffer,"hello world"\n);
+        int rc=write(fd,buffer,strlen(buffer));
+        assert(rc==(strlen(buffer)));
+        fsync(fd);
+        close(fd);
+        return 0;
+}
+
